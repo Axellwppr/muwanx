84261dd20cc30ec6527e0a19382ca0c053d993ec
ttktjmt <tatsuki.tsujimoto@gmail.com>
2026-01-05 01:52:00 +0900

    Extend mujoco types

--- src/muwanx/template/src/types/mujoco.d.ts
+++ src/muwanx/template/src/types/mujoco.d.ts
@@ -4,33 +4,33 @@ import type { MainModule, MjModel, MjData } from 'mujoco-js';
 export type Mujoco = MainModule;
 
 declare module 'mujoco-js' {
-  export interface MujocoFS {
-    analyzePath(path: string, dontResolveLastLink?: boolean): { exists: boolean; isRoot: boolean; error: number };
-  }
+    export interface MujocoFS {
+        analyzePath(path: string, dontResolveLastLink?: boolean): { exists: boolean; isRoot: boolean; error: number };
+    }
 
-  export interface MainModule {
-    mj_forward(model: MjModel, data: MjData): void;
-    mj_step(model: MjModel, data: MjData): void;
-    mjtGeom: {
-      mjGEOM_BOX: { value: number };
-      [key: string]: { value: number };
-    };
-    mjtLightType: {
-      mjLIGHT_DIRECTIONAL: { value: number };
-      mjLIGHT_POINT: { value: number };
-      mjLIGHT_SPOT: { value: number };
-      mjLIGHT_IMAGE: { value: number };
-      [key: string]: { value: number };
-    };
-    mjtTexture: {
-      mjTEXTURE_2D: { value: number };
-      mjTEXTURE_CUBE: { value: number };
-      [key: string]: { value: number };
-    };
-    mjtTextureRole: {
-      mjTEXROLE_RGB: { value: number };
-      mjNTEXROLE: { value: number };
-      [key: string]: { value: number };
-    };
-  }
+    export interface MainModule {
+        mj_forward(model: MjModel, data: MjData): void;
+        mj_step(model: MjModel, data: MjData): void;
+        mjtGeom: {
+            mjGEOM_BOX: { value: number };
+            [key: string]: { value: number };
+        };
+        mjtLightType: {
+            mjLIGHT_DIRECTIONAL: { value: number };
+            mjLIGHT_POINT: { value: number };
+            mjLIGHT_SPOT: { value: number };
+            mjLIGHT_IMAGE: { value: number };
+            [key: string]: { value: number };
+        };
+        mjtTexture: {
+            mjTEXTURE_2D: { value: number };
+            mjTEXTURE_CUBE: { value: number };
+            [key: string]: { value: number };
+        };
+        mjtTextureRole: {
+            mjTEXROLE_RGB: { value: number };
+            mjNTEXROLE: { value: number };
+            [key: string]: { value: number };
+        };
+    }
 }
