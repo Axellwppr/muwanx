29df88d66244f3742afb30fb0a4fd315200db581
ttktjmt <tatsuki.tsujimoto@gmail.com>
2026-01-06 01:03:55 +0900

    Remove mjlab dependency for packaging

--- examples/mjlab/mjlab_integration.py
+++ examples/mjlab/mjlab_integration.py
@@ -3,7 +3,7 @@
 This example demonstrates how to build muwanx projects using mjlab setups.
 """
 
-from mjlab.tasks.registry import load_env_cfg, load_rl_cfg
+# from mjlab.tasks.registry import load_env_cfg, load_rl_cfg
 
 import muwanx as mwx
 
@@ -11,8 +11,8 @@ import muwanx as mwx
 def main():
     builder = mwx.Builder()
     mjlab_project = builder.add_project(name="MJLab Integration Example")  # noqa: F841
-    env_cfg = load_env_cfg("Mjlab-Velocity-Flat-Unitree-G1")  # noqa: F841
-    rl_cfg = load_rl_cfg("Mjlab-Velocity-Flat-Unitree-G1")  # noqa: F841
+    # env_cfg = load_env_cfg("Mjlab-Velocity-Flat-Unitree-G1")  # noqa: F841
+    # rl_cfg = load_rl_cfg("Mjlab-Velocity-Flat-Unitree-G1")  # noqa: F841
 
 
 if __name__ == "__main__":
